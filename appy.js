ef577d6950efead2e7ad3ffe688233e37924f9ad
David <61171213+thatdavidguy@users.noreply.github.com>
2020-06-16 10:38:17 +0800

    Update appy.js

--- appy.js
+++ appy.js
@@ -11,8 +11,7 @@ function resizeScreen() {
 resizeScreen();
 
 isrec = 0
-aomin = 0
-aomax = 0
+amount = 0
 var Recording = document.getElementById("rec_stat");
 function STARTTHERECORDING(){
   if (isrec === 0){
@@ -50,29 +49,82 @@ function STARTTHERECORDING(){
     } 
   }
   else {
-  console.log("Recording is already in session")
+    Recording.innerHTML = "Recording is already in session";
   }
 }
+function openup(fromid){
+  var number = fromid[fromid.length -1];
+  idee = "text" + number;
+  ree = document.getElementById(idee);
+  if (document.getElementById(fromid).value == "open") {
+    document.getElementById("button"+number).value = "close";
+    ree.style.visibility = "visible";
+  }
+  else {
+    document.getElementById("button"+number).value = "open";
+    ree.style.visibility = "hidden";
+  }
+}
+
 function STOPTHERECORDING(){
+  
   Recording.innerHTML = "";
   isrec = 0;
   speechRecognizer.stop();
-  aomin++;
-  console.log(aomin)
   var dive = document.createElement('div');
   document.getElementById("heightofpage").style.height = "200vh" 
-  if (aomin >= 4){
-      notes = document.getElementById("translist2");
-  } else {
-      notes = document.getElementById("translist");
+  notes = document.getElementById("translist2");
+  amount++;
+  if (amount >= 10){
+    document.getElementById("rec_stat").innerHTML ="You cannot have more than 10 notes."
   }
-  var dive = document.createElement('div');
-  dive.style.borderWidth = "2px";
-  dive.style.fontWeight = "lighter";
-  dive.style.marginTop = "10px";
-  dive.style.borderStyle= "solid";
-  dive.style.overflowY= "scroll";
-  dive.innerText += r.textContent;
-  notes.insertBefore(dive, notes.childNodes[0]);
+  ideen =  'text'+amount;
+  buttiden = "button"+amount;
+  dividen = "div"+amount;
+
+  var totaldiv = document.createElement('div');
+  totaldiv.style.borderWidth = "2px"
+  totaldiv.style.borderStyle= "solid";
+  totaldiv.style.marginTop = "20px"
+  totaldiv.setAttribute("id", dividen);
+
+  var diveit = document.createElement('div');
+  diveit.style = "display:flex; flex-direction: row;";
+  diveit.classList.add("outpt");
+
+  var diveitbutton = document.createElement('div');
+  diveitbutton.style ="width: 30%;";
   
+  var diveitinput = document.createElement('input');
+  diveitinput.type = "button";
+  diveitinput.value = "open";
+  diveitinput.setAttribute("id", buttiden);
+  diveitinput.onclick = function() {openup(this.id);};
+  
+  
+
+  diveitbutton.appendChild(diveitinput);
+
+  var diveittitle = document.createElement('div');
+  diveittitle.style = "width: 70%; ";
+  diveittitle.innerHTML = r.innerHTML.substring(0, 10);
+
+  diveit.appendChild(diveitbutton);
+  diveit.appendChild(diveittitle);
+
+
+  var divetex = document.createElement('div');
+  divetex.style.visibility = "hidden";
+  divetex.innerHTML += r.textContent;
+  divetex.style.fontWeight = "lighter";
+  divetex.style.marginTop = "10px"
+  divetex.style.marginLeft = "10px"
+  divetex.style.marginRight = "10px"
+  divetex.style.marginBottom = "10px"
+  divetex.style.overflowY= "scroll";
+  divetex.setAttribute("id", ideen);
+
+  totaldiv.appendChild(diveit);
+  totaldiv.appendChild(divetex);
+  notes.insertBefore(totaldiv, notes.childNodes[0]);
 }
